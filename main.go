8228cc853499e83c3f2cba4122961a2d7b789787
Rollie Ma <rollie.ma@gmail.com>
2016-04-13 23:37:38 -0700

    piglow experimental code for LED control

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+func main() {
+	p, err := piglow.NewPiglow()
+	if err != nil {
+		log.Fatal("Couldn't create a Piglow: ", err)
+	}
+
+	// p.SetLED(0, 0)
+	// p.SetLED(1, 0)
+	p.SetTentacle(0, 255)
+	err = p.Apply()
+	if err != nil { // Apply the changes
+		log.Fatal("Couldn't apply changes: ", err)
+	}
+	time.Sleep(time.Second)
+	p.SetTentacle(0, 0)
+	err = p.Apply()
+	if err != nil { // Apply the changes
+		log.Fatal("Couldn't apply changes: ", err)
+	}
+	time.Sleep(time.Second)
+	p.SetTentacle(1, 255)
+	err = p.Apply()
+	if err != nil { // Apply the changes
+		log.Fatal("Couldn't apply changes: ", err)
+	}
+	time.Sleep(time.Second)
+	p.SetTentacle(1, 0)
+	err = p.Apply()
+	if err != nil { // Apply the changes
+		log.Fatal("Couldn't apply changes: ", err)
+	}
+	time.Sleep(time.Second)
+	p.SetTentacle(2, 255)
+	err = p.Apply()
+	if err != nil { // Apply the changes
+		log.Fatal("Couldn't apply changes: ", err)
+	}
+	time.Sleep(time.Second)
+	p.SetTentacle(2, 0)
+	err = p.Apply()
+	if err != nil { // Apply the changes
+		log.Fatal("Couldn't apply changes: ", err)
+	}
+}
